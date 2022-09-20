239b3438384d0585ec22ff1dd76bbba3d7cb3e7b
Yuta Toyokawa <kppg42@gmail.com>
2022-09-20 21:18:20 +0900

    ヒーローのスマホ最適化

--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -1,90 +1,100 @@
 <template lang="pug">
 section#hero
   img.hero-image(src="@/assets/images/hero.jpeg")
-  h1.main-copy 歳をとってもイケてる男性であり続けるためのパーソナルジム
+  h1.main-copy
+    span 歳をとってもイケてる男性で
+    span あり続けるためのパーソナルジム
   .sub-copy 女性目線の徹底指導で"モテ"ボディメイク
   n-link.top-cta(v-scroll-to="'#reserve'" to)
-    .text オープン前予約で
-    .action オトクな特典をGET!!
+    .cta-set
+      .text オープン前予約で
+      .action オトクな特典をGET!!
     .text 予約フォームはコチラ
 </template>
 
 <style lang="sass" scoped>
 #hero
   position: relative
-  overflow: hidden
   .hero-image
     width: 100%
-    height: 75%
-    object-fit: cover
-    object-position: center center
+    height: auto
+    @include media-breakpoint-down(md)
+      height: 480px
+      object-fit: cover
+      object-position: center center
   .main-copy
     font-size: 24px
     font-weight: bold
     color: white
     line-height: 1.5
-    padding: 15px 25px 15px 30px
+    padding: 15px 40px 15px 30px
     position: absolute
     bottom: 100px
     left: -15px
     margin-bottom: 0
+    overflow: hidden
     z-index: 1
+    @include media-breakpoint-down(md)
+      font-size: 16px
+      bottom: 150px
+      span
+        display: block
     &::before
-      content: ''
-      position: absolute
-      top: 0
-      bottom: 0
-      left: 0
-      right: 0
+      @include slash-bg
       background-color: $accent-color
-      transform: skewX(-20deg)
+      transform: skewX(-20deg) translateX(-15px)
       z-index: -1
   .sub-copy
     font-size: 20px
     font-weight: bold
     color: $accent-color
-    padding: 15px 25px 15px 30px
+    padding: 15px 40px 15px 30px
     position: absolute
     bottom: 25px
     left: -15px
+    overflow: hidden
     z-index: 1
+    @include media-breakpoint-down(md)
+      font-size: 14px
+      bottom: 85px
     &::before
-      content: ''
-      position: absolute
-      top: 0
-      bottom: 0
-      left: 0
-      right: 0
+      @include slash-bg
       background-color: rgba(255, 255, 255, 0.8)
-      transform: skewX(-20deg)
+      transform: skewX(-20deg) translateX(-15px)
       z-index: -1
   .top-cta
-    text-align: right
     font-weight: bold
     color: white
     text-decoration: none
-    padding: 20px 20px 20px 50px
-    overflow: hidden
-    position: absolute
-    bottom: 35px
-    right: 30px
     z-index: 1
-    .text
-      font-size: 20px
-    .action
-      font-size: 32px
-    &::before
-      content: ''
+    @include media-breakpoint-up(lg)
+      text-align: right
+      padding: 20px 20px 20px 50px
+      overflow: hidden
       position: absolute
-      top: 0
-      bottom: 0
-      left: 0
-      right: 0
+      bottom: 35px
+      right: 30px
+      &::before
+        @include slash-bg
+        background: linear-gradient(to right, #FFC226, $accent-color)
+        opacity: 0.8
+        transform: skewX(-20deg) translateX(28px)
+        z-index: -1
+        transition: 0.3s
+      &:hover::before
+        opacity: 1
+      .text
+        font-size: 20px
+      .action
+        font-size: 32px
+    @include media-breakpoint-down(md)
+      display: block
       background: linear-gradient(to right, #FFC226, $accent-color)
-      opacity: 0.8
-      transform: skewX(-20deg) translateX(28px)
-      z-index: -1
-      transition: 0.3s
-    &:hover::before
-      opacity: 1
+      padding: 10px 20px
+      .text
+        display: inline
+        font-size: 14px
+      .action
+        display: inline
+        font-size: 20px
 </style>
