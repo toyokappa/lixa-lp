77834891df4d52d4c624f29e2f9c25c995c2eae3
Yuta Toyokawa <kppg42@gmail.com>
2022-10-01 19:31:01 +0900

    求人票URLの修正

--- components/section/Trainers.vue
+++ components/section/Trainers.vue
@@ -4,7 +4,7 @@ section#trainers.main-section
   .container
     .text-center
       .mb-4 現在採用活動中。今しばらくお待ち下さい。
-      a(href="https://indeedhi.re/3RZhsDI" target="_blank" rel="noopener")
+      a(href="https://indeedhi.re/3dZowRY" target="_blank" rel="noopener")
         img.ads-recruit(src="@/assets/images/ads-recruit.png" alt="求人広告")
 </template>
 
