be420f88e78b7103b563aa69521a41b3aa812654
Yuta Toyokawa <kppg42@gmail.com>
2022-11-02 00:21:08 +0900

    トップ文言の変更

--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -2,8 +2,8 @@
 section#hero
   img.hero-image(src="@/assets/images/hero.jpeg")
   h1.main-copy
-    span 歳をとってもイケてる男性で
-    span あり続けるためのパーソナルジム
+    span 群馬初！女性トレーナーのみ
+    span 男性向け完全個室パーソナルジム
   .sub-copy 女性目線の徹底指導で"モテ"ボディメイク
   .open-info
     div
